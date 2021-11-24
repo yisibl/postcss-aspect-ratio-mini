8806f0311cc109d75938a9c3d64849d4114c05fb
龚春铭 <gongchunming@wanzhuiot.com>
2021-11-24 21:58:56 +0800

    add prettier ignore, remove unused tests

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,18 @@ var postcss = require('gulp-postcss')
 var tape = require('gulp-tape')
 var tapDiff = require('tap-diff')
 
-
 var files = ['index.js', './test/*.js', 'gulpfile.js']
 
-gulp.task('test', function() {
-  return gulp.src('test/*.js')
-    .pipe(tape({
-      reporter: tapDiff()
-    }))
+gulp.task('test', function () {
+  return gulp.src('test/*.js').pipe(
+    tape({
+      reporter: tapDiff(),
+    })
+  )
 })
 
 gulp.task('default', ['test'])
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch(files, ['test'])
 })
--- test/test.js
+++ test/test.js
@@ -19,9 +19,8 @@ function compareFixtures(t, name, msg, opts, postcssOpts) {
   postcssOpts = postcssOpts || {}
   postcssOpts.from = filename('fixtures/' + name)
   opts = opts || {}
-  var actual = postcss()
-    .use(plugin(opts))
-    .process(read(postcssOpts.from), postcssOpts).css
+  // prettier-ignore
+  var actual = postcss().use(plugin(opts)).process(read(postcssOpts.from), postcssOpts).css
   var expected = read(filename('fixtures/' + name + '.expected'))
   fs.writeFileSync(filename('fixtures/' + name + '.actual'), actual)
   t.equal(actual, expected, msg)
